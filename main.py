d90a33128cf4a10e202b71050d5346750506352f
Jade Sauvé <64500436+jade-sauve@users.noreply.github.com>
2021-03-05 14:33:36 -0800

    intro

---
+++ main.py
@@ -0,0 +1,27 @@
+"""
+Jade Sauve
+Mary Margaret Stoll
+
+March 2021
+
+Final project for Climate Dynamics
+presented to Kyle Armour and Gerard Roe
+
+"""
+
+## Import packages ##
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import sys
+pht = os.path.abspath('/Users/jadesauve/Documents/Python/scripts/2_layer_carbon/')
+if pht not in sys.path:
+    sys.path.append(pht)
+from modules import *
+
+
+#### Parameters ####
+
+
+###################
