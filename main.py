60ee2668c678a17c480c7e0ddd5da70a9c853d97
Jade Sauvé <64500436+jade-sauve@users.noreply.github.com>
2021-03-05 17:05:21 -0800

    Update main.py
    started coding FAIR

--- main.py
+++ main.py
@@ -23,5 +23,61 @@ from modules import *
 
 #### Parameters ####
 
+# model constants from Millar et al 2017
+a = (0.2173, 0.2240, 0.2824, 0.2763) #fraction of carbon emissions entering the reservoir 
+tau = (1e6, 394.4, 36.54, 4.304) # decay time constant for the pool 9(r)
+r0 = # preindustrial iIRF100
+rc = # Increase in iIRF100 with cumulative carbon uptake
+rt = # Increase in iIRF100 with warming
+
+# parameters
+E = # annual CO2 emissions (ppm/yr)
+dt = # timestep size
+timesteps = # number of timestep
+
+# initial conditions
+C0 = # preindustrial CO2 concentration 
+R = [0,0,0,0]
+
+# reservoir labels
+res_dic = {0:'geology',1:'deep',2:'thermocline',3:'ml'}
 
 ###################
+
+
+# make a datframe to hold the reservoir change with time
+R = np.DataFrame(index=timesteps, columns=[0,1,2,3])
+# make a df to hold the temperature anomaly with time
+T = np.DataFrame(index = timesteps, columns=['T_sfc','T_deep'])
+
+
+
+# FAIR
+
+# find IRF100 from previous T,C
+# atmopsheric [CO2]
+C = C0 + sum(R.loc[t]) # where R is an array with shape = 4
+# accumulated carbon
+Cacc = E - (C-C0) # this is summed  over time.... check
+# 
+irf100 = r0 + rc*Cacc + rt*T.loc[t,'T_sfc']
+
+# use fsolve to find the roots
+def IRF100(alpha):
+	RHS = 0
+	for i in range(0,4):
+		RHS = RHS + alpha*a[i]*tau[i]*(1-np.exp(-100/alpha*tau[i]))
+
+	return RHS - irf100
+
+alpha = fsolve(IRF100,alpha0)
+
+
+
+
+
+
+
+
+
+
